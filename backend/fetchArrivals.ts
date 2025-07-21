2d74908932b0868b57f00e66c2e7877e6fc8eb22
Bill Haigh <bill.haigh@softwire.com>
2025-07-21 13:47:04 +0100

    it works but needs tidying up

--- backend/fetchArrivals.ts
+++ backend/fetchArrivals.ts
@@ -1,4 +1,4 @@
-export default async function fetchArrivals(stopcode:string) {
+export async function fetchArrivals(stopcode:string) {
     try {
         const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopcode}/Arrivals?app_key=5d5ebbce60f9408d9116c189191fe788`);
         if (!response.ok) {
@@ -7,12 +7,53 @@ export default async function fetchArrivals(stopcode:string) {
 
         const data = await response.json();
         console.log('Fetched arrivals:', data);
-        data.map((bus: any)=> {
+        const processedArrivals = await Promise.all(
+            data.map((bus: any)=> {
             return {destinationName: bus.destinationName, expectedArrival: bus.expectedArrival, lineName: bus.lineName, timeToStation: bus.timeToStation}
-        })
-        return data;
+        }))
+        console.log('Processed arrivals:', processedArrivals);
+        return processedArrivals;
     } catch (error) {
         console.error('Error fetching arrivals:', error);
     }
 }
 
+export async function fetchPostcode(postcode: string) {
+    try {
+        const response = await fetch(`https://api.postcodes.io/postcodes/${postcode}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log('Fetched postcode:', data);
+        const location = {longitude: data.result.longitude, latitude: data.result.latitude};
+        return location;
+    } catch (error) {
+        console.error('Error fetching postcode:', error);
+    }
+}
+
+export async function fetchStopsByPostcode(postcode: string) {
+    try {
+        const location = await fetchPostcode(postcode);
+        if (!location) {
+            throw new Error('Location not found');
+        }
+        
+        const response = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${location.latitude}&lon=${location.longitude}&stopTypes=NaptanPublicBusCoachTram&app_key=5d5ebbce60f9408d9116c189191fe788`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log('Fetched stops by postcode:', data);
+        const stops = data.stopPoints.map((stop: any) => {
+            return stop.naptanId
+        });
+        console.log('Processed stops:', stops);
+        return stops;
+    } catch (error) {
+        console.error('Error fetching stops by postcode:', error);
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import { useState } from 'react';
-import fetchArrivals from '../backend/fetchArrivals';
+import { useEffect, useState } from "react";
+import {
+  fetchArrivals,
+  fetchPostcode,
+  fetchStopsByPostcode,
+} from "../backend/fetchArrivals";
 
 interface Arrival {
   lineName: string;
@@ -8,49 +12,89 @@ interface Arrival {
   timeToStation: number;
 }
 
-
 function App() {
   const [arrivals, setArrivals] = useState<Arrival[]>([]);
-  const [stopcode, setStopcode] = useState('');
+  const [stopcodes, setStopcodes] = useState([]);
+  const [postcode, setPostcode] = useState("");
 
-  async function fetchArrivalsFromAPI(stopcode: string) {
-    const data: any = await fetchArrivals(stopcode);
-    setArrivals(data)
+  async function fetchArrivalsFromAPI(postcode: string) {
+    const stops: any = await fetchStopsByPostcode(postcode);
+    setStopcodes(stops);
+    const data = await Promise.all(
+      stops.map(async (stopcode: string) => {
+        return await fetchArrivals(stopcode);
+      })
+    );
+    console.log("this is arrivals", data);
+    setArrivals(data);
   }
 
   return (
-        <div>
-          <h1 className="text-3xl font-bold underline text-center text-cyan-600 m-4">BusBoard</h1>
-          <input
-            type="text"
-            placeholder="Enter Stop Code"
-            onChange={(e) => setStopcode(e.target.value)}
-            className="border border-gray-300 rounded p-2 mb-4 w-full"
-          />
-          <button className='border border-gray-300 rounded p-2 mb-4 w-full' onClick={() => fetchArrivalsFromAPI(stopcode)}>Fetch Arrivals</button>
-          <div>
-            {arrivals ? (arrivals.length > 5 ? arrivals.slice(4).map((bus: Arrival) => {
-              return (
-                <div key={bus.lineName} className="border border-gray-300 rounded p-2 mb-4">
-                  <h2 className="text-xl font-semibold">{bus.lineName}</h2>
-                  <p>Destination: {bus.destinationName}</p>
-                  <p>Expected Arrival: {new Date(bus.expectedArrival).toLocaleTimeString()}</p>
-                  <p>Time to Station: {bus.timeToStation} seconds</p>
-                </div>
-              );
-            }) : arrivals.map((bus: Arrival) => {
-              return (
-                <div key={bus.lineName} className="border border-gray-300 rounded p-2 mb-4">
-                  <h2 className="text-xl font-semibold">{bus.lineName}</h2>
-                  <p>Destination: {bus.destinationName}</p>
-                  <p>Expected Arrival: {new Date(bus.expectedArrival).toLocaleTimeString()}</p>
-                  <p>Time to Station: {bus.timeToStation} seconds</p>
-                </div>
-              );
-            })) : 'No arrivals data available.'}
-          </div>
-        </div>
-  )
+    <div>
+      <h1 className="text-3xl font-bold underline text-center text-cyan-600 m-4">
+        BusBoard
+      </h1>
+      <input
+        type="text"
+        placeholder="Enter Post Code"
+        onChange={(e) => setPostcode(e.target.value.replace(" ", ""))}
+        className="border border-gray-300 rounded p-2 mb-4 w-full"
+      />
+      <button
+        className="border border-gray-300 rounded p-2 mb-4 w-full"
+        onClick={() => fetchArrivalsFromAPI(postcode)}
+      >
+        Fetch Arrivals
+      </button>
+      <div>
+        {arrivals.map((stop) => {
+          return (
+            <div>
+              {stop.length > 0
+                ? stop.length > 5
+                  ? stop.slice(4).map((bus: Arrival) => {
+                      return (
+                        <div
+                          key={bus.lineName}
+                          className="border border-gray-300 rounded p-2 mb-4"
+                        >
+                          <h2 className="text-xl font-semibold">
+                            {bus.lineName}
+                          </h2>
+                          <p>Destination: {bus.destinationName}</p>
+                          <p>
+                            Expected Arrival:{" "}
+                            {new Date(bus.expectedArrival).toLocaleTimeString()}
+                          </p>
+                          <p>Time to Station: {bus.timeToStation} seconds</p>
+                        </div>
+                      );
+                    })
+                  : stop.map((bus: Arrival) => {
+                      return (
+                        <div
+                          key={bus.lineName}
+                          className="border border-gray-300 rounded p-2 mb-4"
+                        >
+                          <h2 className="text-xl font-semibold">
+                            {bus.lineName}
+                          </h2>
+                          <p>Destination: {bus.destinationName}</p>
+                          <p>
+                            Expected Arrival:{" "}
+                            {new Date(bus.expectedArrival).toLocaleTimeString()}
+                          </p>
+                          <p>Time to Station: {bus.timeToStation} seconds</p>
+                        </div>
+                      );
+                    })
+                : "No arrivals data available."}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
